eefb5347a849dc415033d3ec22ae10c2fde815b6
Dan Hollingsworth <hollingsworthd@objectcomputing.com>
2022-05-19 18:10:48 -0400

    Show picocli error handling

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,10 +1,36 @@
 package com.example;
 
+import io.micronaut.configuration.picocli.PicocliRunner;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
 
-public class Application {
+@CommandLine.Command(name = "divide", header = {
+        "@|green A CLI for division |@",
+        "@|green Pipe in your numerator and denominator |@",
+        "@|green Get the result of the division in stdout |@"},
+        description = "A command line division operator",
+        exitCodeListHeading = "Exit Codes:%n",
+        exitCodeList = { " 0:Successful program execution.",
+                "64:Invalid input: an unknown option or invalid parameter was specified.",
+                "70:Execution exception: an exception occurred while executing the business logic."})
+public class Application implements Runnable {
 
+    private static final Logger logger =LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        PicocliRunner.execute(Application.class, args);
+    }
+
+    public Application(){
+        System.out.println("Creating application");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running command");
+        new CommandLine(this).usage(System.err);
+        logger.error("Invalid args", new IllegalArgumentException());
+        System.exit(64);
     }
 }
